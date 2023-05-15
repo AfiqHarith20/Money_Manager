bf75c8f38b3c7b7b1e7cea29d99716b132b6d5e3
Afiq Harith <afiqharith15@gmail.com>
2023-05-15 17:26:07 +0800

    update for new section

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,18 +1,18 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
 
-Future<double> getExchangeRate(String fromCurrency, String toCurrency) async {
-  final String url =
-      "https://openexchangerates.org/api/latest.json?app_id=573cc675cce44bef92d46f222efebc19&base=$fromCurrency";
-  final response = await http.get(Uri.parse(url));
-  if (response.statusCode == 200) {
-    final data = json.decode(response.body);
-    final exchangeRate = data['rates'];
-    final rate = exchangeRate[toCurrency];
-    if (rate != null) {
-      return rate.toDouble();
-    }
-  }
-  throw Exception(
-      'Failed to get exchange rate from $fromCurrency to $toCurrency');
-}
+// Future<double> getExchangeRate(String fromCurrency, String toCurrency) async {
+//   final String url =
+//       "https://openexchangerates.org/api/latest.json?app_id=573cc675cce44bef92d46f222efebc19&base=$fromCurrency";
+//   final response = await http.get(Uri.parse(url));
+//   if (response.statusCode == 200) {
+//     final data = json.decode(response.body);
+//     final exchangeRate = data['rates'];
+//     final rate = exchangeRate[toCurrency];
+//     if (rate != null) {
+//       return rate.toDouble();
+//     }
+//   }
+//   throw Exception(
+//       'Failed to get exchange rate from $fromCurrency to $toCurrency');
+// }
